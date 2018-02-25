63c556d742942f3ddd3d66eb27dfb86577238eac
sumalatha2020 <infosumalatha@gmail.com>
2018-02-25 16:57:17 +0530

    Add files via upload
    Python code to accept the comma seperated values from user and returns the values in list format

---
+++ PrintListFromCommaSeperatedValues.py
@@ -0,0 +1,5 @@
+#Accepts comma,seperated values from user
+values = input("Input comma seprated numbers : ")
+#split with "," and stores the values in list variable as list of values
+list = values.split(",")
+print('List : ',list)
